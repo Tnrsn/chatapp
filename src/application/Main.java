ab07f47bc527b59d81229d1ba0542d90b03d4705
Taner Şan <Tnrsn16@gmail.com>
2026-05-04 18:05:52 +0300

    Load UI from FXML and apply stylesheet
    Add Main.fxml layout files (src/, project root and bin/) and update Main.java to load the UI via FXMLLoader (use Parent root). Replace the manual Group root/colored Scene with an FXML-backed Scene and attach application.css; fullscreen and manual color setup have been commented out. Compiled Main.class was also updated.

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,8 +2,10 @@ package application;
 	
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
@@ -18,10 +20,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 	
-		Group root = new Group();
+		Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
 
-		Scene scene = new Scene(root, Color.rgb(32, 30, 31, 1.0));
-		stage.setFullScreen(true);
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//		, Color.rgb(32, 30, 31, 1.0)
+//		stage.setFullScreen(true);
 		
 		stage.setScene(scene);
 		stage.show();
