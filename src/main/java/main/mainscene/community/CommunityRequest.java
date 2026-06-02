3375f79ab9b3c5ab0e02d38d3e424add1578746f
Taner Şan <Tnrsn16@gmail.com>
2026-06-02 22:42:45 +0300

    Add community creation UI and API client
    Introduces community creation flow: adds CommunityAPIClient to POST create requests (uses Jackson and ServerManagement for address/token) and a CommunityRequest model. Updates CreateCommunity.fxml to bind controls (serverName, serverDescription, isPublic) and minor FXML cleanup. CreateCommunityController now references the new fields, collects tags and calls CommunityAPIClient.createCommunity on create; also simplifies stylesheet loading. Build metadata (maven input list) updated to include the new sources.

---
+++ src/main/java/main/mainscene/community/CommunityRequest.java
@@ -0,0 +1,35 @@
+package main.mainscene.community;
+
+import java.util.List;
+
+public class CommunityRequest {
+    private String name;
+    private String description;
+    private boolean isPublic;
+    private List<String> tags;
+    
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isPublic() {
+		return isPublic;
+	}
+	public void setPublic(boolean isPublic) {
+		this.isPublic = isPublic;
+	}
+	public List<String> getTags() {
+		return tags;
+	}
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+}
--- src/main/java/main/mainscene/community/CreateCommunityController.java
+++ src/main/java/main/mainscene/community/CreateCommunityController.java
@@ -9,11 +9,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import main.mainscene.community.tags.CommunityTagsController;
-import main.mainscene.peopleblock.PeopleBlockController;
 
 public class CreateCommunityController {
 
@@ -23,6 +24,13 @@ public class CreateCommunityController {
 	private VBox tagContainer;
 	@FXML
 	private TextField serverTagsField;
+	
+	@FXML
+	private TextField serverName;
+	@FXML
+	private CheckBox isPublic;
+	@FXML
+	private TextArea serverDescription;
 	
 	private final List<String> tags = new ArrayList<>();
 	
@@ -60,12 +68,9 @@ public class CreateCommunityController {
 	        tags.add(tag);
 
 	        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main/mainscene/community/tags/CommunityTags.fxml"));
+	        
 	        Parent block = loader.load();
-
-	        block.getStylesheets().add(
-	            getClass()
-	                .getResource("/main/mainscene/community/tags/CommunityTags.css")
-	                .toExternalForm());
+	        block.getStylesheets().add(getClass().getResource("/main/mainscene/community/tags/CommunityTags.css").toExternalForm());
 
 	        CommunityTagsController controller = loader.getController();
 	        controller.getTagName().setText(tag);
@@ -82,7 +87,8 @@ public class CreateCommunityController {
 	@FXML
 	public void CreateServer(ActionEvent event)
 	{
-		System.out.println("Create Server");
+	    CommunityAPIClient.createCommunity(serverName.getText(), serverDescription.getText(), isPublic.isSelected(), 
+	    		new ArrayList<>(tags));
 	}
 	
 	public VBox getTagContainer()
